8ef3a0a179afa5de41b35402e1290a54255ac431
Mishin Dmitry <dmishin.dev@gmail.com>
2023-09-28 16:30:00 +0300

    added log s3 conf

--- config/s3-client.js
+++ config/s3-client.js
@@ -3,6 +3,16 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+console.log("s3 conf", {
+  endpoint: process.env.DO_SPACES_URL,
+  region: process.env.DO_SPACES_REGION,
+  forcePathStyle: false,
+  credentials: {
+    accessKeyId: process.env.DO_CDN_ACCESS_KEY,
+    secretAccessKey: process.env.DO_CDN_SECRET_ACCESS_KEY,
+  },
+});
+
 const s3Client = new S3({
   endpoint: process.env.DO_SPACES_URL,
   region: process.env.DO_SPACES_REGION,
